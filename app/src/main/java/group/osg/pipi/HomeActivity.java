753c6550e040d5156775682955b3ecd221ef67a1
yueguoyunygy <17862003625@163.com>
2017-05-14 14:47:49 +0800

    add toast

--- app/src/main/java/group/osg/pipi/HomeActivity.java
+++ app/src/main/java/group/osg/pipi/HomeActivity.java
@@ -2,13 +2,17 @@ package group.osg.pipi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
     }
 }
